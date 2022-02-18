58c3cc7bd69299d71e6f81aa3ef486720f4bc860
Nejdancs <nejdancs@gmail.com>
2022-02-18 11:59:35 +0000

    Deploying to gh-pages from @ Nejdancs/team-project-group13@4956936c48bf87470ca701f03910e9192464901a 🚀

--- src.d7eb4de7.js
+++ src.d7eb4de7.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-group13/src.41f46fda.js.map
+//# sourceMappingURL=/team-project-group13/src.d7eb4de7.js.map
